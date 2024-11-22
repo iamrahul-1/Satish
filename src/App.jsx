bb0f813aac667e60c9608be7e310d63f14b4f704
Roshan <139259259+Roshnnnnn@users.noreply.github.com>
2024-11-23 05:04:54 +0530

    width in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="w-[80%] max-w-5xl">
+        <Navbar />
+        <Outlet />
+      </div>
+    </div>
   );
 }
 
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,17 @@
 import { motion } from "framer-motion";
 import Slider from "./Slider";
+import img1 from "../../assets/w1.jpg";
+import img2 from "../../assets/w2.jpg";
+import img3 from "../../assets/w3.jpg";
+import img4 from "../../assets/w4.jpg";
+import img5 from "../../assets/w5.jpg";
+import img6 from "../../assets/w6.jpg";
 
 const Home = () => {
   return (
     <div>
       <Slider />
-      <div className="mt-16 bottom-0 left-0 w-full text-center">
+      <div className="mt-16 bottom-0 left-0 w-full text-center flex flex-col items-center justify-center">
         <motion.h6
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
@@ -22,11 +28,31 @@ const Home = () => {
         >
           Creative Elements
         </motion.h4>
-        <motion.p className="text-xs md:text-xs lg:text-xs font-light leading-relaxed text-center mx-4 p-4 bg-black/30 rounded-lg shadow-lg">
+        <motion.p className="text-xs md:text-xs lg:text-xs font-light leading-relaxed text-center mx-4 p-4 w-[70%] rounded-lg uppercase justify-center">
           Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
           commodo ligula eget dolor. Aenean massa. Cum sociis Theme natoque
           penatibus et magnis dis parturient montes viverra quis, feugiat.
         </motion.p>
+      </div>
+      <div className="mt-16 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 mx-6">
+        <motion.div className="relative">
+          <img src={img1} alt="Description" className="w-full h-auto" />
+        </motion.div>
+        <motion.div className="relative">
+          <img src={img2} alt="Description" className="w-full h-auto" />
+        </motion.div>
+        <motion.div className="relative">
+          <img src={img3} alt="Description" className="w-full h-auto" />
+        </motion.div>
+        <motion.div className="relative">
+          <img src={img4} alt="Description" className="w-full h-auto" />
+        </motion.div>
+        <motion.div className="relative">
+          <img src={img5} alt="Description" className="w-full h-auto" />
+        </motion.div>
+        <motion.div className="relative">
+          <img src={img6} alt="Description" className="w-full h-auto" />
+        </motion.div>
       </div>
     </div>
   );
